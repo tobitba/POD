10367a0adf7c90c72ecc87885213a01367a2d5e9
tobias <tpugliano@itba.edu.ar>
2025-03-26 15:53:37 -0300

    se borro todo el 4 aca va. xd

--- concurrency-iii/src/test/java/ar/edu/itba/pod/concurrency/iii/e4/ScheduledTest.java
+++ concurrency-iii/src/test/java/ar/edu/itba/pod/concurrency/iii/e4/ScheduledTest.java
@@ -2,22 +2,31 @@ package ar.edu.itba.pod.concurrency.iii.e4;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.util.concurrent.*;
 
 
 public class ScheduledTest {
+
+    private Runnable printTimestamp = () -> System.out.println(new Date());
+    private static class Stop implements Runnable {
+        ScheduledFuture<?> future;
+        public Stop(ScheduledFuture<?> future) {this.future = future;}
+        @Override
+        public void run() {
+            future.cancel(true);
+        }
+    }
 
     @Test
     public final void test() throws InterruptedException, ExecutionException {
         final ScheduledExecutorService executorService = Executors
                 .newSingleThreadScheduledExecutor();
         try {
-
+            ScheduledFuture<?> future = executorService.scheduleWithFixedDelay(printTimestamp, 0, 10, TimeUnit.SECONDS);
+            executorService.schedule(new Stop(future), 60, TimeUnit.SECONDS);
         } finally {
-
+            Thread.sleep(100000);
             executorService.shutdown();
             executorService.awaitTermination(2, TimeUnit.SECONDS);
             executorService.shutdownNow();
