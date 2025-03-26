c4a1232b4d5b5cc55a8d9266648ecdbc2b92b68e
tobias <tpugliano@itba.edu.ar>
2025-03-26 12:42:27 -0300

    se agregaron consultas.

--- TP2_Programacion_Concurrente_Thread_Safety/src/main/java/pruebaFuture.java
+++ TP2_Programacion_Concurrente_Thread_Safety/src/main/java/pruebaFuture.java
@@ -9,14 +9,14 @@ public class pruebaFuture {
 
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(MAX_FIBONACCI/2 + 1);
+        ExecutorService executor = Executors.newFixedThreadPool(MAX_FIBONACCI/2 + 1); //TODO: porque pide try with resource
 
         List<Future<result>> futures = IntStream
                 .range(0, MAX_FIBONACCI)
                 .mapToObj(number -> executor.submit(new prueba(number)))
-                .toList();
+                .toList();   //TODO: eficiencia?
         for(Future<result> future : futures) {
-            result resultado = future.get();
+            result resultado = future.get(); //TODO: como úedo hacer si quiero recuperar el primero que termina y no ir en orden de insert en la lista
             System.out.printf("fibo(%d) = %d%n", resultado.n(), resultado.result());
         }
 
